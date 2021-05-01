8e66633d8b41d34a3c7822fa2c6898c1dbb0c285
mehran-mehrafarin <mehran.mehrafarin9270@gmail.com>
2021-05-02 01:43:00 +0430

    initial spring boot

---
+++ src/main/java/com/gamesys/cryptocoin/CryptoCoinApplication.java
@@ -0,0 +1,13 @@
+package com.gamesys.cryptocoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoCoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoCoinApplication.class, args);
+	}
+
+}
